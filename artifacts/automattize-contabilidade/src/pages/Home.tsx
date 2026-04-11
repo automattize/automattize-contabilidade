6eb85f41b61408cc58d8b02b8bac43de4fb444ea
automattize <automattize.dp@gmail.com>
2026-04-11 08:59:42 -0300

    123

--- artifacts/automattize-contabilidade/src/pages/Home.tsx
+++ artifacts/automattize-contabilidade/src/pages/Home.tsx
@@ -551,11 +551,24 @@ function PropostaDrawer({
               <div>
                 <label style={labelStyle}>
                   <span style={{ color: "#dc2626" }}>*</span> Telefone / Celular
-               
+                </label>
+                <input
+                  type="text"
+                  placeholder="(61) 99180-4169"
+                  value={form.telefone}
+                  onChange={(e) => {
+                    let digits = e.target.value.replace(/\D/g, "");
+                    if (digits.length > 11) digits = digits.slice(0, 11);
+                    let v = digits;
+                    if (digits.length <= 10) {
+                      v = digits.replace(/^(\d{2})(\d{4})(\d{0,4})$/, "($1) $2-$3");
+                    } else {
+                      v = digits.replace(/^(\d{2})(\d{5})(\d{0,4})$/, "($1) $2-$3");
+                    }
                     setForm((p) => ({ ...p, telefone: v }));
                   }}
                   style={inputStyle}
-                  maxLength={16}
+                  maxLength={INPUT_LIMITS.telefone}
                 />
               </div>
 
